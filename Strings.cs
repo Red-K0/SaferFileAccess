47f0f921057706bd53287c6bd524f37ca5cc1e54
Red-K0 <114494949+Red-K0@users.noreply.github.com>
2024-01-10 11:12:16 +0200

    Add project files.

---
+++ Strings.cs
@@ -0,0 +1,29 @@
+﻿#pragma warning disable CA1050 //Declare types in namespaces
+public static partial class SafeFile
+{
+#pragma warning disable CA1707 //Identifiers should not contain underscores
+
+	#region Fallbacks
+	/// <summary> Defaults to the 'My Documents' folder. (<see cref="Environment.SpecialFolder.MyDocuments"/>) </summary>
+	public static string FALLBACK_PATH { get; set; } = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}";
+	/// <summary> Defaults to the 'Fallback.txt' file in the 'My Documents' folder. (<see cref="Environment.SpecialFolder.MyDocuments"/>) </summary>
+	public static string FALLBACK_FILE { get; set; } = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\Fallback.txt";
+	public static IEnumerable<string> IENUMFALLBACK_STR { get; set; } = new List<string>()
+	{
+		"This is fallback text",
+		"Don't touch it",
+		"Or do"
+	};
+	#endregion
+
+	#region Error Messages
+	private const string SAFEPATH_BLANK = "The given 'safepath' parameter was blank but not null, defaulted to the path in FALLBACK_FILE.";
+	private const string SAFEPATH_FAIL = "The given 'safepath' parameter was inaccessible, defaulted to the path in FALLBACK_FILE.";
+
+	private const string SAFECONTENT_EMPTY = "An empty IEnumerable was given in the 'safecontents' parameter, defaulted to the contents of IENUMFALLBACK_STR.";
+	#endregion
+
+#pragma warning restore CA1707
+}
+
+#pragma warning restore
---
+++ sFile.cs
@@ -0,0 +1,66 @@
+﻿#pragma warning disable IDE0079 //Remove unnessecary supression
+#pragma warning disable CA1050 //Declare types in namespaces
+public static partial class SafeFile
+{
+	/// <summary> Appends lines to a file, and then closes the file. If the specified file foes not exist, this method creates a file, writes the specified lines to the file, then closes the file. </summary>
+	/// <param name="path"> The directory of the file to modify or create. If invalid, will be set to <see cref="FALLBACK_FILE"/>, or <paramref name="safepath"/> if given. </param>
+	/// <param name="contents"> The contents to insert into the file. If invalid, will be set to <see cref="IENUMFALLBACK_STR"/>, or <paramref name="safecontents"/> if given. </param>
+	/// <param name="safepath"> The path to fall back to if <paramref name="path"/> fails. Resets to <see cref="FALLBACK_FILE"/> if invalid. </param>
+	/// <param name="safecontents"> The contents to use if <paramref name="contents"/> fails. Resets to <see cref="IENUMFALLBACK_STR"/> if invalid. </param>
+	/// <returns> True if all operations proceeded without the use of fallbacks, otherwise returns false. </returns>
+	public static bool AppendAllLines(string path, IEnumerable<string> contents, string? safepath = null, IEnumerable<string>? safecontents = null)
+	{
+		bool safetyfail = false;
+
+		// safepath override check
+		safepath ??= FALLBACK_FILE;
+		if (string.IsNullOrWhiteSpace(safepath))
+		{
+			safetyfail = true;
+			Console.Error.WriteLine(SAFEPATH_BLANK);
+			safepath = FALLBACK_FILE;
+		}
+
+		// safepath safety check
+		#pragma warning disable CA1031 //Do not catch general exception types
+		try { File.Open(safepath, FileMode.OpenOrCreate).Dispose(); File.Delete(safepath); }
+		catch (Exception)
+		{
+			safetyfail = true;
+			Console.Error.WriteLine(SAFEPATH_FAIL);
+			safepath = FALLBACK_FILE;
+		}
+		#pragma warning restore
+
+		// null or empty path check
+		if (string.IsNullOrWhiteSpace(path)) path = safepath;
+
+		// path validity check
+		#pragma warning disable CA1031 //Do not catch general exception types
+		try { File.Open(path, FileMode.OpenOrCreate).Dispose(); File.Delete(safepath); }
+		catch (Exception) { path = safepath; }
+		#pragma warning restore
+
+		// null or empty contents check
+		if (contents == null || contents.Any())
+		{
+			if (safecontents != null && !safecontents.Any()) contents = safecontents;
+		}
+		if (contents == null || contents.Any())
+		{
+			if (safecontents != null)
+			{
+				safetyfail = true;
+				Console.Error.WriteLine(SAFECONTENT_EMPTY);
+			}
+			safecontents = IENUMFALLBACK_STR;
+			contents = safecontents;
+		}
+
+		// Final method call
+		File.AppendAllLines(path, contents);
+		return !safetyfail;
+	}
+}
+
+#pragma warning restore
